2ddd95e9079b3f9d99f86f84cbec3f24b8d70214
Pankaj Pareek <110030329+pankjpareek@users.noreply.github.com>
2022-08-22 15:09:27 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const { connect } = require('http2');
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "3175",
-    database: "testdb"
+    host: "www.remotemysql.com",
+    user: "lAKQV8LSMJ",
+    password: "6HNz80BJfP",
+    database: "lAKQV8LSMJ"
 })
 // conn.query("select * from newUser where username='pankajpareek' AND password='331302'",(e,result)=>{
 //     if(e)console.log(e);
